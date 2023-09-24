dfca2b95e7d432d45eb4892483755fbcd4838713
Kelvin Mue <kelvinmue@gmail.com>
2023-09-24 10:44:42 +0300

    Added review.py

---
+++ review.py
@@ -0,0 +1,20 @@
+class Review:
+    reviews = []
+
+    def __init__(self, customer, restaurant, rating):
+        self.customer = customer
+        self.restaurant = restaurant
+        self.rating = rating
+
+    def get_rating(self):
+        return self.rating
+
+    @classmethod
+    def all(cls):
+        return cls.reviews
+
+    def get_customer(self):
+        return self.customer
+
+    def get_restaurant(self):
+        return self.restaurant
